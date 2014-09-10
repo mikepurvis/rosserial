1353e73d6a77536a7a6872d823044745c54b61bf
Mike Purvis <mpurvis@clearpathrobotics.com>
2014-09-10 00:07:01 -0400

    Split configuring client job from adding targets.

--- rosserial_client/cmake/rosserial_client-extras.cmake
+++ rosserial_client/cmake/rosserial_client-extras.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-function(rosserial_client_generate_ros_lib)
+function(rosserial_generate_ros_lib)
   cmake_parse_arguments(make_libraries "" "PACKAGE;SCRIPT" "" ${ARGN}) 
   if(NOT make_libraries_PACKAGE)
     set(make_libraries_PACKAGE rosserial_client)
@@ -20,27 +20,30 @@ function(rosserial_client_generate_ros_lib)
   set(${PROJECT_NAME}_ROS_LIB_DIR "${PROJECT_BINARY_DIR}/ros_lib" PARENT_SCOPE)
 endfunction()
 
-function(rosserial_client_add_client)
-  cmake_parse_arguments(client "" "DIRECTORY;TOOLCHAIN_FILE" "TARGETS" ${ARGN})
+function(rosserial_configure_client)
+  cmake_parse_arguments(client "" "DIRECTORY;TOOLCHAIN_FILE" "" ${ARGN})
   if(NOT client_DIRECTORY)
     message(SEND_ERROR "rosserial_client_add_client called without DIRECTORY argument.")
-  endif()
-  if(NOT client_TARGETS)
-    message(SEND_ERROR "rosserial_client_add_client called with no TARGETS specified.")
   endif()
 
   # Create a build tree directory for configuring the client's CMake project.
   file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/${client_DIRECTORY})
-  add_custom_target(${PROJECT_NAME}_${client_DIRECTORY} ALL
+  add_custom_target(${PROJECT_NAME}_${client_DIRECTORY}
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/${client_DIRECTORY}
     COMMAND ${CMAKE_COMMAND} ${PROJECT_SOURCE_DIR}/${client_DIRECTORY}
       -DROS_LIB_DIR=${${PROJECT_NAME}_ROS_LIB_DIR}
       -DPACKAGE_SOURCE_DIR=${PROJECT_SOURCE_DIR}
       -DEXECUTABLE_OUTPUT_PATH=${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION}
       -DCMAKE_TOOLCHAIN_FILE=${client_TOOLCHAIN_FILE}
-    COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/${client_DIRECTORY} -- ${client_TARGETS}
   )
+  add_dependencies(${PROJECT_NAME}_${client_DIRECTORY} ${PROJECT_NAME}_ros_lib)
+endfunction()
 
-  # Depend on the ros lib being built.
-  add_dependencies(${PROJECT_NAME}_firmware ${PROJECT_NAME}_ros_lib)
+function(rosserial_add_client_target client_directory client_target)
+  add_custom_target(${PROJECT_NAME}_${client_directory}_${client_target} ${ARGN}
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/${client_directory}
+    COMMAND ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/${client_directory} -- ${client_target}
+  )
+  add_dependencies(${PROJECT_NAME}_${client_directory}_${client_target}
+                   ${PROJECT_NAME}_${client_directory})
 endfunction()
